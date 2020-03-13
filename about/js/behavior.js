0d57609a72d7fd2daf58ccfecbc6d5f33e647557
Geatnium <bushclovershigh92948@gmail.com>
2020-03-13 21:58:27 +0900

    ローディング画面を追加

--- about/js/behavior.js
+++ about/js/behavior.js
@@ -4,6 +4,29 @@
 
     let scrollPosi = 0;
 
+    $(function () {
+        var h = $(window).height();
+        $('#page').css('display', 'none');
+        $('#loader-bg ,#loader').height(h).css('display', 'block');
+    });
+
+    $(window).on('load', function () { //全ての読み込みが完了したら実行
+        setTimeout(stopload, 1000);
+    });
+
+    //10秒たったら強制的にロード画面を非表示
+    $(function () {
+        setTimeout(stopload, 10000);
+    });
+
+    function stopload() {
+        $('#loader-bg').delay(1000).fadeOut(800);
+        $('#loaderanim').delay(500).fadeOut(300);
+        $('#nowloading').delay(500).fadeOut(300);
+        $('#nowloading').text('Completed!');
+        $('#page').css('display', 'block');
+    }
+
     window.onload = function () {
         var f = 0;
         for (f = 0; f < 10; f++) {
